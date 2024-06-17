27277d91a282945f2816a5329d344f976f90c393
manzil-infinity180 <rahulvs2809@gmail.com>
2024-06-17 19:50:26 +0530

    changed

--- backend/e2e/admin/adminRoute.spec.js
+++
@@ -1,6 +0,0 @@
-
-describe.skip('2 + 3', () => {
-    test('hello',() => {
-        expect(5).toBe(5);
-    })
-})
--- backend/utils/setDataInRedis.js
+++
@@ -1,9 +0,0 @@
-const setDataInRedis = async (data)=>{
-    const key = req.originalUrl || req.url;
-    try{
-      await client.set(key,JSON.stringify({data : data}),'ex',86400);
-    }catch(err){
-      console.error("Redis Error "+err);
-    }
-}
-module.exports = setDataInRedis;
